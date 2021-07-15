369e313cf34b52fe5115186adf0df75163054b87
Aidil83 <aidilrozi83@gmail.com>
2021-07-14 22:53:56 -0500

    Created and setup database.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import { graphqlHTTP } from 'express-graphql';
-import { createConnection } from 'typeorm';
+import express from "express";
+import cors from "cors";
+import { graphqlHTTP } from "express-graphql";
+import { createConnection } from "typeorm";
 
 const main = async () => {
+	await createConnection({
+		type: "mysql",
+		database: "graphql_crud",
+		username: "root",
+		password: "password",
+		logging: true,
+		synchronize: false,
+		entities: [],
+	});
+
 	const app = express();
 	app.use(cors());
 	app.use(express.json());
@@ -11,8 +21,9 @@ const main = async () => {
 	//   schema,
 	//   graphiql: true,
 	// }))
+
 	app.listen(3001, () => {
-		console.log('Server running on port 3001.');
+		console.log("Server running on port 3001.");
 	});
 };
 
